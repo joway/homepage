e0424671b4ffe135ed28022404b2732b21f37ab8
Joway Wang <joway.w@gmail.com>
2018-10-24 11:59:19 +0800

    add analytics.js

---
+++ analytics.js
@@ -0,0 +1,17 @@
+// get domain
+var domain = window.location.host;
+// get userId
+var userId = window.localStorage.getItem('userId');
+if (!userId) {
+  userId = Math.floor(Math.random() * 1000000000000);
+  window.localStorage.setItem('userId', userId)
+  gtag('event', 'user_register', { 'domain': domain });
+} else {
+  gtag('event', 'user_login', { 'domain': domain });
+}
+console.log('welcome', userId, '!');
+window.dataLayer = window.dataLayer || [];
+function gtag() { dataLayer.push(arguments); }
+gtag('js', new Date());
+gtag('config', 'UA-53624533-8');
+gtag('set', { 'user_id': userId });
