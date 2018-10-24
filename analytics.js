5581b28b40d87022501bbf31159006008947e48b
Joway Wang <joway.w@gmail.com>
2018-10-24 12:13:15 +0800

    fix analysis

--- analytics.js
+++ analytics.js
@@ -1,17 +1,17 @@
+window.dataLayer = window.dataLayer || [];
+function gtag() { dataLayer.push(arguments); }
+gtag('js', new Date());
+gtag('config', 'UA-53624533-8');
 // get domain
 var domain = window.location.host;
 // get userId
 var userId = window.localStorage.getItem('userId');
+var event_type = 'user_login'
 if (!userId) {
   userId = Math.floor(Math.random() * 1000000000000);
-  window.localStorage.setItem('userId', userId)
-  gtag('event', 'user_register', { 'domain': domain });
-} else {
-  gtag('event', 'user_login', { 'domain': domain });
+  window.localStorage.setItem('userId', userId);
+  event_type = 'user_register';
 }
-console.log('welcome', userId, '!');
-window.dataLayer = window.dataLayer || [];
-function gtag() { window.dataLayer.push(arguments); }
-gtag('js', new Date());
-gtag('config', 'UA-53624533-8');
 gtag('set', { 'user_id': userId });
+gtag('event', event_type, { 'domain': domain });
+console.log('welcome', userId, '!');
