5f1e86238f8a8ebf281faa22005e3096342d6b7c
Joway Wang <joway.w@gmail.com>
2018-10-26 20:41:25 +0800

    add ga label

--- analytics.js
+++ analytics.js
@@ -1,17 +1,19 @@
 window.dataLayer = window.dataLayer || [];
-function gtag() { dataLayer.push(arguments); }
-gtag('js', new Date());
-gtag('config', 'UA-53624533-8');
+function gtag() {
+  dataLayer.push(arguments);
+}
+gtag("js", new Date());
+gtag("config", "UA-53624533-8");
 // get domain
 var domain = window.location.host;
 // get userId
-var userId = window.localStorage.getItem('userId');
-var event_type = 'user_login'
+var userId = window.localStorage.getItem("userId");
+var event_type = "user_login";
 if (!userId) {
   userId = Math.floor(Math.random() * 1000000000000);
-  window.localStorage.setItem('userId', userId);
-  event_type = 'user_register';
+  window.localStorage.setItem("userId", userId);
+  event_type = "user_register";
 }
-gtag('set', { 'user_id': userId });
-gtag('event', event_type, { 'domain': domain });
-console.log('welcome', userId, '!');
+gtag("set", { user_id: userId });
+gtag("event", event_type, { method: domain });
+console.log("welcome", userId, "!");
